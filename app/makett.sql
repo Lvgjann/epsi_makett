ecbccb8feabb165b78c61e5856b0d1e5f5daa150
Maxime <maxime.audy@cheops.fr>
2020-09-09 15:52:03 +0200

    add password to user

--- app/makett.sql
+++ app/makett.sql
@@ -92,6 +92,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `nom` varchar(250) NOT NULL,
   `prenom` varchar(250) NOT NULL,
   `photo` varchar(250) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `id_groupe` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
